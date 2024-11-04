2d2410f04035f373241b28e4c43c1d51057b3a65
AbdellaouiHajar1 <abdellaouihajar22@ump.ac.ma>
2024-11-04 22:32:21 +0100

    4 commit

--- src/main/java/org/example/Hello_world.java
+++ src/main/java/org/example/Hello_world.java
@@ -3,4 +3,7 @@ package org.example;
 public class Hello_world {
     public String h;
     public String kk;
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
